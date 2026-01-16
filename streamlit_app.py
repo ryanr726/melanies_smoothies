14ffef381000c496d358421d07f8df9f35811351
ryanr726 <ryanreich25@yahoo.com>
2026-01-16 12:02:00 -0500

    Update streamlit_app.py
    added full json reponse

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 # New section to display smoothiefroot nutrtion info
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
